6a4545b8b8f456b1d1ef0a2788d940f310023853
Nikita Novoselov <sarkomath@outlook.com>
2020-12-10 22:00:32 +1000

    check passed

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,11 +7,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
   @Test
-  void findMax() {
+  void findMaxFirst() {
     StatisticsService service = new StatisticsService();
 
     long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
     long expected = 12;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void findMaxSecond() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 15, 11, 12};
+    long expected = 15;
 
     long actual = service.findMax(incomesInBillions);
 
